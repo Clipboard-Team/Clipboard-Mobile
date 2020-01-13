b9c3624fddab039195975231d90e926040a27a41
Xavier La Rosa <xavier.a.larosa@gmail.com>
2020-01-13 16:35:37 -0500

    added edit member controller on storyboard and connected

---
+++ Clipboard/Clipboard/EditMemberController.swift
@@ -0,0 +1,19 @@
+//
+//  EditMemberController.swift
+//  Clipboard
+//
+//  Created by Xavier La Rosa on 1/13/20.
+//  Copyright © 2020 Xavier La Rosa. All rights reserved.
+//
+
+import UIKit
+
+class EditMemberController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
