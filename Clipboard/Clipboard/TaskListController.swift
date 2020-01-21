0252bdb1d0bb06557429e46d8381b103aa14ad1c
Xavier La Rosa <xavier.a.larosa@gmail.com>
2020-01-21 11:43:20 -0500

    changed to only 1 section, going with one section on tasklistcontroller works much better

--- Clipboard/Clipboard/TaskListController.swift
+++ Clipboard/Clipboard/TaskListController.swift
@@ -10,8 +10,7 @@ import UIKit
 
 class TaskListController: UIViewController {
     private var taskToEditOrDelete = Task(title: "Default", status: "Default", difficulty: "Default")
-    var taskLists = [ExpandableTasks]()
-    var filteredTasks = [ExpandableTasks]()
+    var filteredTasks = [Task]()
     @IBOutlet weak var tableView: UITableView!
     lazy var searchController: UISearchController = {
         let s = UISearchController(searchResultsController: nil)
@@ -27,7 +26,7 @@ class TaskListController: UIViewController {
     
     func filterContentForSearchBar(searchText: String, scope: String = "All"){
         guard let allTasks = Constants.currProject.getTeam()?.getTasks() else {return}
-        filteredTasks[0].tasks = allTasks.filter({(task: Task)->Bool in
+        filteredTasks = allTasks.filter({(task: Task)->Bool in
             let doesTaskMatch = (scope == "All") || (task.getStatus() == scope) || (task.getDifficulty() == scope)
             if(isSearchBarTextEmpty()){
                 return doesTaskMatch
@@ -59,20 +58,8 @@ class TaskListController: UIViewController {
         tableView.rowHeight = 50
         tableView.register(TaskCell.self, forCellReuseIdentifier: "taskCell")
         
-        guard let toDos = Constants.currProject.getTeam()?.getToDoTasks() else {return}
-        guard let inProgs = Constants.currProject.getTeam()?.getInProgressTasks() else {return}
-        guard let halts = Constants.currProject.getTeam()?.getHaltedTasks() else {return}
-        guard let dones = Constants.currProject.getTeam()?.getDoneTasks() else {return}
-        
-        taskLists.append(ExpandableTasks(isExpanded: true, tasks: toDos))
-        taskLists.append(ExpandableTasks(isExpanded: true, tasks: inProgs))
-        taskLists.append(ExpandableTasks(isExpanded: true, tasks: halts))
-        taskLists.append(ExpandableTasks(isExpanded: true, tasks: dones))
-        
-        filteredTasks.append(ExpandableTasks(isExpanded: true, tasks: toDos))
-        filteredTasks.append(ExpandableTasks(isExpanded: true, tasks: inProgs))
-        filteredTasks.append(ExpandableTasks(isExpanded: true, tasks: halts))
-        filteredTasks.append(ExpandableTasks(isExpanded: true, tasks: dones))
+   
+        print("total size: "+String(Constants.currProject.getTeam()?.getTasks().count ?? 0))
     }
 
     @IBAction func filterButtonTapped(_ sender: Any) {
@@ -92,86 +79,84 @@ class TaskListController: UIViewController {
 
 extension TaskListController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        if !taskLists[section].isExpanded {
-            return 0
-        }
-        
         if(isFiltering()){
             return filteredTasks.count
         }
-        return taskLists[section].tasks.count
+        guard let allTasks = Constants.currProject.getTeam()?.getTasks() else{return 0}
+
+        return allTasks.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "taskCell") as! TaskCell
         let task: Task
         if(isFiltering()){
-            task = filteredTasks[indexPath.section].tasks[indexPath.row]
+            task = filteredTasks[indexPath.row]
 
         } else{
-            task = taskLists[indexPath.section].tasks[indexPath.row]
+            guard let allTasks = Constants.currProject.getTeam()?.getTasks() else{return UITableViewCell()}
+            task = allTasks[indexPath.row]
         }
         cell.set(task: task)
-               return cell
+        return cell
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return Constants.statuses.count
+        return 1
     }
     
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        let tail = " Tasks"
-        switch section {
-        case 0:
-            return Constants.statuses[0]+tail
-        case 1:
-            return Constants.statuses[1]+tail
-        case 2:
-            return Constants.statuses[2]+tail
-        case 3:
-            return Constants.statuses[3]+tail
-        default:
-            return "Default Header"
-        }
-    }
+//    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        let tail = " Tasks"
+//        switch section {
+//        case 0:
+//            return Constants.statuses[0]+tail
+//        case 1:
+//            return Constants.statuses[1]+tail
+//        case 2:
+//            return Constants.statuses[2]+tail
+//        case 3:
+//            return Constants.statuses[3]+tail
+//        default:
+//            return "Default Header"
+//        }
+//    }
     
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        
-        let button = UIButton(type: .system)
-        button.setTitle("Close", for: .normal)
-        button.setTitleColor(.black, for: .normal)
-        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
-        button.addTarget(self, action: #selector(handleExpandClose), for: .touchUpInside)
-        button.tag = section
-        
-        return button
-    }
-    
-    @objc func handleExpandClose(button: UIButton) {
-        print("Trying to expand and close section...")
-        
-        let section = button.tag
-         
-        // we'll try to close the section first by deleting the rows
-        var indexPaths = [IndexPath]()
-        for row in taskLists[section].tasks.indices {
-            print(0, row)
-            let indexPath = IndexPath(row: row, section: section)
-            indexPaths.append(indexPath)
-        }
-        
-        let isExpanded = taskLists[section].isExpanded
-        taskLists[section].isExpanded = !isExpanded
-        
-        button.setTitle(isExpanded ? "Open" : "Close", for: .normal)
-        
-        if isExpanded {
-            tableView.deleteRows(at: indexPaths, with: .fade)
-        } else {
-            tableView.insertRows(at: indexPaths, with: .fade)
-        }
-    }
+//    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+//
+//        let button = UIButton(type: .system)
+//        button.setTitle("Close", for: .normal)
+//        button.setTitleColor(.black, for: .normal)
+//        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+//        button.addTarget(self, action: #selector(handleExpandClose), for: .touchUpInside)
+//        button.tag = section
+//
+//        return button
+//    }
+//
+//    @objc func handleExpandClose(button: UIButton) {
+//        print("Trying to expand and close section...")
+//
+//        let section = button.tag
+//
+//        // we'll try to close the section first by deleting the rows
+//        var indexPaths = [IndexPath]()
+//        for row in taskLists[section].tasks.indices {
+//            print(0, row)
+//            let indexPath = IndexPath(row: row, section: section)
+//            indexPaths.append(indexPath)
+//        }
+//
+//        let isExpanded = taskLists[section].isExpanded
+//        taskLists[section].isExpanded = !isExpanded
+//
+//        button.setTitle(isExpanded ? "Open" : "Close", for: .normal)
+//
+//        if isExpanded {
+//            tableView.deleteRows(at: indexPaths, with: .fade)
+//        } else {
+//            tableView.insertRows(at: indexPaths, with: .fade)
+//        }
+//    }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.beginUpdates()
         tableView.endUpdates()
